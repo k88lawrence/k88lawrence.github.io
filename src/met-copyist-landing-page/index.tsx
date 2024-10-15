6187c294a2458b1ec5e1c9d2ff2b55e5aa58fa0b
Kathryn Lawrence <kathrynfosterlawrence@gmail.com>
2024-10-14 20:09:55 -0400

    bad idea?

---
+++ src/met-copyist-landing-page/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./met-copyist-landing-page.css";
+
+const MetCopyistLandingPage = () => {
+  return (
+    <div className="met-copyist-body">
+      <div className="met-copyist-header">
+        <h1>landing page</h1>
+        <p>
+          Hello! I'm Kate Lawrence, an artist from Brooklyn. I'm currently
+          copying Georges de la Tour's <em>The Penitent Magdalen</em> as a part
+          of the Met's Copyist Program. You can find more information about me
+          and the program at the links below!
+        </p>
+      </div>
+      <div className="met-copyist-body">
+        <h1>landing page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default MetCopyistLandingPage;
