d17eec7ff57dbbab68bf81059b14fccc4ad47cd5
Kathryn Lawrence <kathrynfosterlawrence@gmail.com>
2024-11-18 22:29:28 -0500

    add tiktok

---
+++ src/components/socials.tsx
@@ -0,0 +1,27 @@
+import InstagramLogo from "../images/instagram-logo.svg";
+import CaraLogo from "../images/cara-logo.svg";
+import TikTokLogo from "../images/tiktok-logo.svg";
+
+const Socials = () => (
+  <>
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://www.instagram.com/katelawrence.art/"
+    >
+      <img className="social-icon" alt="Instagram" src={InstagramLogo} />
+    </a>
+    <a
+      target="_blank"
+      rel="noreferrer"
+      href="https://www.tiktok.com/@k88lawrence"
+    >
+      <img className="social-icon" alt="TikTok" src={TikTokLogo} />
+    </a>
+    <a target="_blank" rel="noreferrer" href="https://cara.app/katelawrence">
+      <img className="social-icon" alt="Cara" src={CaraLogo} />
+    </a>
+  </>
+);
+
+export default Socials;
