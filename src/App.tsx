90cb751254c72d28764b058d2f2032dbc8bd0e64
Kathryn Lawrence <kathrynfosterlawrence@gmail.com>
2024-11-18 22:29:28 -0500

    add tiktok

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import React from "react";
 import "./App.css";
 
-import InstagramIcon from "./images/instagram-icon.svg";
-import CaraIcon from "./images/cara-icon.svg";
+import Socials from "./components/socials";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Coming Soon</h1>
-      </header>
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Coming Soon</h1>
+    </header>
 
-      <div className="footer">
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.instagram.com/katelawrence.art/"
-        >
-          <img className="social-icon" alt="Instagram" src={InstagramIcon} />
-        </a>
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://cara.app/katelawrence"
-        >
-          <img className="social-icon" alt="Cara" src={CaraIcon} />
-        </a>
-      </div>
+    <div className="footer">
+      <Socials />
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
--- src/met-copyist-landing-page/index.tsx
+++ src/met-copyist-landing-page/index.tsx
@@ -1,80 +1,71 @@
-import React from "react";
 import "./met-copyist-landing-page.css";
 
 import SkullIcon from "../images/skull-icon.jpg";
-import InstagramIcon from "../images/instagram-icon.svg";
-import CaraIcon from "../images/cara-icon.svg";
 
-const MetCopyistLandingPage = () => {
-  return (
-    <div className="met-copyist-landing-page-outer">
-      <div className="met-copyist-landing-page-inner">
-        <div className="met-copyist-header">
-          <img className="icon-img" src={SkullIcon} alt="icon" />
-          <h1>Kate Lawrence</h1>
-          <p>
-            Hello! I'm Kate, an artist from Brooklyn. I'm currently copying
-            Georges de la Tour's{" "}
-            <em>
-              <a
-                className="text-link"
-                href="https://www.metmuseum.org/art/collection/search/436839"
-                target="_blank"
-                rel="noreferrer"
-              >
-                The Penitent Magdalen
-              </a>
-            </em>{" "}
-            as a part of the Met's Copyist Program. You can find more
-            information about me and the program at the links below!
-          </p>
-        </div>
-        <div className="met-copyist-body">
-          <a
-            className="met-copyist-link"
-            target="_blank"
-            rel="noreferrer"
-            href="https://www.metmuseum.org/events/programs/met-creates/copyist-program"
-          >
-            The Met Copyist Program
-          </a>
-          <a
-            target="_blank"
-            className="met-copyist-link"
-            rel="noreferrer"
-            href="https://www.instagram.com/katelawrence.art/"
-          >
-            See more of my work on Instagram
-          </a>
-          <a
-            target="_blank"
-            className="met-copyist-link"
-            rel="noreferrer"
-            href="https://ko-fi.com/katelawrenceart"
-          >
-            Like what I'm doing? Buy me a coffee!
-          </a>
-        </div>
+import Socials from "../components/socials";
+
+const MetCopyistLandingPage = () => (
+  <div className="met-copyist-landing-page-outer">
+    <div className="met-copyist-landing-page-inner">
+      <div className="met-copyist-header">
+        <img className="icon-img" src={SkullIcon} alt="icon" />
+        <h1>Kate Lawrence</h1>
+        <p>
+          Hello! I'm Kate, an artist from Brooklyn. I'm currently copying
+          Georges de la Tour's{" "}
+          <em>
+            <a
+              className="text-link"
+              href="https://www.metmuseum.org/art/collection/search/436839"
+              target="_blank"
+              rel="noreferrer"
+            >
+              The Penitent Magdalen
+            </a>
+          </em>{" "}
+          as a part of the Met's Copyist Program. You can find more information
+          about me and the program at the links below!
+        </p>
       </div>
-      <div className="met-copyist-footer">
+      <div className="met-copyist-body">
+        <a
+          className="met-copyist-link"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.metmuseum.org/events/programs/met-creates/copyist-program"
+        >
+          The Met Copyist Program
+        </a>
         <a
           target="_blank"
+          className="met-copyist-link"
           rel="noreferrer"
           href="https://www.instagram.com/katelawrence.art/"
         >
-          <img className="social-icon" alt="Instagram" src={InstagramIcon} />
+          See more of my work on Instagram
         </a>
         <a
           target="_blank"
-          className="social-icon"
+          className="met-copyist-link"
           rel="noreferrer"
-          href="https://cara.app/katelawrence"
+          href="https://www.tiktok.com/@k88lawrence"
         >
-          <img className="social-icon" alt="Cara" src={CaraIcon} />
+          Check out my TikTok
+        </a>
+        <a
+          target="_blank"
+          className="met-copyist-link"
+          rel="noreferrer"
+          href="https://ko-fi.com/katelawrenceart"
+        >
+          Like what I'm doing? Buy me a coffee!
         </a>
       </div>
     </div>
-  );
-};
+    <div className="met-copyist-footer">
+      <Socials />
+    </div>
+  </div>
+);
 
 export default MetCopyistLandingPage;
