105a79ae99ed42414f5694b600237cb0cd7d1689
Kathryn Lawrence <kathrynfosterlawrence@gmail.com>
2026-05-04 22:05:17 -0400

    added more images

--- src/drawings-page.tsx
+++ src/drawings-page.tsx
@@ -4,6 +4,10 @@ import ImageObject from "./image-object";
 import SelfPortraitHare from "./images/drawings/self-portrait-hare.jpg";
 import SelfPortraitSphinx from "./images/drawings/self-portrait-sphinx.jpg";
 import SheWolf from "./images/drawings/she-wolf.jpg";
+import LookAtTheMoon from "./images/drawings/look-at-the-moon.jpg";
+import SelfPortraitMandragora from "./images/drawings/self-portrait-mandragora.jpg";
+import Sirin from "./images/drawings/sirin.jpg";
+import DeathsHeadMoth from "./images/drawings/deaths-head-moth.jpg";
 
 const DrawingsPage = () => (
   <Layout>
@@ -15,14 +19,36 @@ const DrawingsPage = () => (
       <p>4.75” x 6.625”</p>
       <p>graphite on paper</p>
     </ImageObject>
-    <ImageObject image={SheWolf} title="The She Wolf">
+
+    <ImageObject image={DeathsHeadMoth} title="Death's Head Moth">
       <p>2025</p>
+      <p>8.375” x 5.625”</p>
+      <p>graphite on toned paper</p>
+    </ImageObject>
+    <ImageObject image={SheWolf} title="The She Wolf">
+      <p>2024</p>
       <p>6.5” x 11”</p>
       <p>graphite on paper</p>
     </ImageObject>
     <ImageObject image={SelfPortraitSphinx} title="Self Portrait as a Sphinx">
       <p>2024</p>
       <p>6.25” x 4.5”</p>
+      <p>graphite on paper</p>
+    </ImageObject>
+    <ImageObject
+      image={LookAtTheMoon}
+      title="Look at the Moon Over Your Left Shoulder"
+    >
+      <p>2022</p>
+      <p>3.5” x 5.25”</p>
+      <p>graphite on paper</p>
+    </ImageObject>
+    <ImageObject
+      image={SelfPortraitMandragora}
+      title="Self Portrait as a Mandragora"
+    >
+      <p>2021</p>
+      <p>7.75” x 11.5”</p>
       <p>graphite on paper</p>
     </ImageObject>
   </Layout>
--- src/paintings-page/index.tsx
+++ src/paintings-page/index.tsx
@@ -3,14 +3,22 @@ import ImageObject from "../image-object";
 
 import Skull1 from "../images/paintings/skull1.jpg";
 import Skull2 from "../images/paintings/skull2.jpg";
+import Skull3 from "../images/paintings/skull3.jpg";
 import Strawberries from "../images/paintings/strawberries.jpg";
 import Tomatoes from "../images/paintings/tomatoes.jpg";
 import TopoChicoBottle from "../images/paintings/topo-chico-bottle.jpg";
+import MilkPitcher from "../images/paintings/milk-pitcher.jpg";
+import Eggs from "../images/paintings/eggs.jpg";
 
 const PaintingsPage = () => (
   <Layout>
-    <ImageObject image={TopoChicoBottle} title="Topo Chico Bottle">
+    <ImageObject image={Skull3} title="Skull">
       <p>2026</p>
+      <p>5” x 5”</p>
+      <p>oil on canvas</p>
+    </ImageObject>
+    <ImageObject image={TopoChicoBottle} title="Topo Chico Bottle">
+      <p>2025</p>
       <p>5” x 7”</p>
       <p>oil on paper</p>
     </ImageObject>
@@ -29,10 +37,20 @@ const PaintingsPage = () => (
       <p>5.125” x 4”</p>
       <p>oil on paper</p>
     </ImageObject>
+    <ImageObject image={MilkPitcher} title="Milk Pitcher with Leaves">
+      <p>2025</p>
+      <p>5.125” x 4”</p>
+      <p>oil on paper</p>
+    </ImageObject>
     <ImageObject image={Skull1} title="Skull">
       <p>2025</p>
       <p>5.25” x 6”</p>
       <p>oil on unstretched canvas</p>
+    </ImageObject>
+    <ImageObject image={Eggs} title="Eggs">
+      <p>2025</p>
+      <p>4.5” x 3.75”</p>
+      <p>oil on paper</p>
     </ImageObject>
   </Layout>
 );
