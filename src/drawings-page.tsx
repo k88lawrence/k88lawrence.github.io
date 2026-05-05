3a743533899fc47b585bf054875919b4dfe2e4be
Kathryn Lawrence <kathrynfosterlawrence@gmail.com>
2026-05-04 22:09:37 -0400

    no sirin

--- src/drawings-page.tsx
+++ src/drawings-page.tsx
@@ -6,7 +6,6 @@ import SelfPortraitSphinx from "./images/drawings/self-portrait-sphinx.jpg";
 import SheWolf from "./images/drawings/she-wolf.jpg";
 import LookAtTheMoon from "./images/drawings/look-at-the-moon.jpg";
 import SelfPortraitMandragora from "./images/drawings/self-portrait-mandragora.jpg";
-import Sirin from "./images/drawings/sirin.jpg";
 import DeathsHeadMoth from "./images/drawings/deaths-head-moth.jpg";
 
 const DrawingsPage = () => (
@@ -19,7 +18,6 @@ const DrawingsPage = () => (
       <p>4.75” x 6.625”</p>
       <p>graphite on paper</p>
     </ImageObject>
-
     <ImageObject image={DeathsHeadMoth} title="Death's Head Moth">
       <p>2025</p>
       <p>8.375” x 5.625”</p>
